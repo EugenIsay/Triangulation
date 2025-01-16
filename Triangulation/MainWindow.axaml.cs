0fbbda19ddd3b2dd330256eb303fe50611393b76
EugenIsay <zheka4311@gmail.com>
2025-01-16 11:32:01 +0300

    0.1.1

--- Triangulation/MainWindow.axaml.cs
+++ Triangulation/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Input;
 using System.Collections.Generic;
 
@@ -6,9 +7,10 @@ namespace Triangulation
 {
     public partial class MainWindow : Window
     {
+        Ellipse Ellipse { get; set; }
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
             AddHandler(DragDrop.DragOverEvent, DragOver);
         }
         private async void OnPointerPressed(object? sender, PointerPressedEventArgs e)
@@ -20,9 +22,12 @@ namespace Triangulation
         private void DragOver(object? sender, DragEventArgs e)
         {
             e.DragEffects = DragDropEffects.Move;
+            if (e.Source.GetType().Name != "Canvas")
+                Ellipse = e.Source as Ellipse;
             var mousePos = e.GetPosition(canvas);
-            Canvas.SetLeft(First, mousePos.X);
-            Canvas.SetTop(First, mousePos.Y);
+            Canvas.SetLeft(Ellipse, mousePos.X - Ellipse.Width/2);
+            Canvas.SetTop(Ellipse, mousePos.Y - Ellipse.Width / 2);
+
             coord.Text = mousePos.X.ToString() + "|" + mousePos.Y.ToString();
         }
     }
